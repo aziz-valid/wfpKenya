56b472eee4e8c5190364c29a69516dc9a2483f44
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-08-05 09:43:32 +0100

    create dataset documentation

---
+++ R/data.R
@@ -0,0 +1,28 @@
+################################################################################
+#
+#' Raw household dataset from WFP Kenya CSP outcome monitoring survey
+#'
+#' A data.frame with 656 columns and 3059 rows of per household observations.
+#'
+#' @source WFP Kenya CSP outcome monitoring survey data collection from
+#'   15-29 August 2019
+#'
+#'
+#
+################################################################################
+"household"
+
+################################################################################
+#
+#' Codebook for raw household dataset from WFP Kenya CSP outcome monitoring
+#' survey
+#'
+#' A data.frame with 2 columns and 651 rows corresponding to the variables in
+#' the raw \code{household} dataset.
+#'
+#'
+#
+################################################################################
+"codebookHousehold"
+
+
