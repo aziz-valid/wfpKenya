3722f8e7a98503f2368a4e95c4464d72b742d73f
Ernest Guevarra <ernest@validmeasures.org>
2019-08-07 23:32:43 +0100

    update data documentation

--- R/data.R
+++ R/data.R
@@ -2,7 +2,8 @@
 #
 #' Raw household dataset from WFP Kenya CSP outcome monitoring survey
 #'
-#' A data.frame with 656 columns and 3057 rows of per household observations.
+#' @format A data.frame with 656 columns and 3057 rows of per household
+#'   observations.
 #'
 #' @source WFP Kenya CSP outcome monitoring survey data collection from
 #'   15-29 August 2019
@@ -17,7 +18,13 @@
 #' Codebook for raw household dataset from WFP Kenya CSP outcome monitoring
 #' survey
 #'
-#' A data.frame with 649 rows and 3 columns
+#' @format A data.frame with 649 rows and 3 columns.
+#'
+#' \describe{
+#' \item{\code{variable}}{Variable name}
+#' \item{\code{question}}{Question/variable description}
+#' \item{\code{choices}}{Choice of responses}
+#' }
 #'
 #
 ################################################################################
@@ -28,7 +35,7 @@
 #
 #' Raw roster dataset from WFP Kenya CSP outcome monitoring survey
 #'
-#' A data.frame with 16520 rows and 37 columns
+#' @format A data.frame with 16520 rows and 37 columns
 #'
 #' @source WFP Kenya CSP outcome monitoring survey data collection from
 #'   15-29 August 2019
@@ -43,7 +50,12 @@
 #' Codebook for raw roster dataset from WFP Kenya CSP outcome monitoring
 #' survey
 #'
-#' A data.frame with 28 rows and 3 columns
+#' @format A data.frame with 28 rows and 3 columns
+#' \describe{
+#' \item{\code{variable}}{Variable name}
+#' \item{\code{question}}{Question/variable description}
+#' \item{\code{choices}}{Choice of responses}
+#' }
 #'
 #
 ################################################################################
@@ -54,7 +66,7 @@
 #
 #' Raw individual dataset from WFP Kenya CSP outcome monitoring survey
 #'
-#' A data.frame with 12240 rows and 141 columns
+#' @format A data.frame with 12240 rows and 141 columns
 #'
 #' @source WFP Kenya CSP outcome monitoring survey data collection from
 #'   15-29 August 2019
@@ -69,9 +81,60 @@
 #' Codebook for raw individual dataset from WFP Kenya CSP outcome monitoring
 #' survey
 #'
-#' A data.frame with 97 rows and 3 columns
+#' @format A data.frame with 97 rows and 3 columns
+#'
+#' \describe{
+#' \item{\code{variable}}{Variable name}
+#' \item{\code{question}}{Question/variable description}
+#' \item{\code{choices}}{Choice of responses}
+#' }
 #'
 #
 ################################################################################
 "codebookIndividual"
 
+
+################################################################################
+#
+#' Primary sampling unit population dataset
+#'
+#' @format A data.frame with 202 rows and 2 columns.
+#'
+#' @source WFP Kenya CSP outcome monitoring survey data collection from
+#'   15-29 August 2019
+#'
+#
+################################################################################
+"psuData"
+
+
+################################################################################
+#
+#' Raw community dataset from WFP Kenya CSP outcome monitoring survey
+#'
+#' @format A data.frame with 202 rows and 23 columns
+#'
+#' @source WFP Kenya CSP outcome monitoring survey data collection from
+#'   15-29 August 2019
+#'
+#
+################################################################################
+"community"
+
+
+################################################################################
+#
+#' Codebook for raw community dataset from WFP Kenya CSP outcome monitoring
+#' survey
+#'
+#' @format A data.frame with 13 rows and 3 columns
+#'
+#' \describe{
+#' \item{\code{variable}}{Variable name}
+#' \item{\code{question}}{Question/variable description}
+#' \item{\code{choices}}{Choice of responses}
+#' }
+#'
+#
+################################################################################
+"codebookCommunity"
