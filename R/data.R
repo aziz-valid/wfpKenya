b086b07dc55fc0ae4df689f278a4868c7ada238d
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-08-07 16:54:16 +0100

    update data.R

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #
 #' Raw household dataset from WFP Kenya CSP outcome monitoring survey
 #'
-#' A data.frame with 656 columns and 3059 rows of per household observations.
+#' A data.frame with 656 columns and 3057 rows of per household observations.
 #'
 #' @source WFP Kenya CSP outcome monitoring survey data collection from
 #'   15-29 August 2019
@@ -17,10 +17,61 @@
 #' Codebook for raw household dataset from WFP Kenya CSP outcome monitoring
 #' survey
 #'
-#'
+#' A data.frame with 649 rows and 3 columns
 #'
 #
 ################################################################################
 "codebookHousehold"
 
+
+################################################################################
+#
+#' Raw roster dataset from WFP Kenya CSP outcome monitoring survey
+#'
+#' A data.frame with 16520 rows and 37 columns
+#'
+#' @source WFP Kenya CSP outcome monitoring survey data collection from
+#'   15-29 August 2019
+#'
+#
+################################################################################
+"roster"
+
+
+################################################################################
+#
+#' Codebook for raw roster dataset from WFP Kenya CSP outcome monitoring
+#' survey
+#'
+#' A data.frame with 28 rows and 3 columns
+#'
+#
+################################################################################
+"codebookRoster"
+
+
+################################################################################
+#
+#' Raw individual dataset from WFP Kenya CSP outcome monitoring survey
+#'
+#' A data.frame with 12240 rows and 141 columns
+#'
+#' @source WFP Kenya CSP outcome monitoring survey data collection from
+#'   15-29 August 2019
+#'
+#
+################################################################################
+"individual"
+
+
+################################################################################
+#
+#' Codebook for raw individual dataset from WFP Kenya CSP outcome monitoring
+#' survey
+#'
+#' A data.frame with 97 rows and 3 columns
+#'
+#
+################################################################################
+"codebookIndividual"
 
