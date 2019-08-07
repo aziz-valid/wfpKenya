2c91748d60cef282190b666af02cd4fb70065908
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-08-07 16:54:34 +0100

    update processData.R

--- data-raw/processData.R
+++ data-raw/processData.R
@@ -66,7 +66,6 @@ codebook$variable <- as.character(codebook$variable)
 codebook$question <- as.character(codebook$question)
 codebook$choices <- as.character(codebook$choices)
 
-
 ################################################################################
 #
 # Process various datasets from ODK
@@ -81,12 +80,12 @@ temp <- get_wfp_data(id = "household",
                      filename = "household")
 
 ## Pull repeat data
-roster <- get_wfp_repeat(id = "household",
-                         username = "validmeasures",
-                         password = "6Y2-8yK-Nmk-Lbf",
-                         start = "2019-07-01",
-                         filename = "household",
-                         repeat.name = "hh_repeat")
+temp2 <- get_wfp_repeat(id = "household",
+                        username = "validmeasures",
+                        password = "6Y2-8yK-Nmk-Lbf",
+                        start = "2019-07-01",
+                        filename = "household",
+                        repeat.name = "hh_repeat")
 
 coreColumns <- names(temp)[c(1:19, 1185:1188, ncol(temp))]
 
@@ -102,27 +101,98 @@ household <- subset(household, select = c(-identify_head, -consent1, -fcsInstruc
 ## Save household data into package
 usethis::use_data(household, overwrite = TRUE)
 
-codebookHousehold <- codebook[codebook$variable %in% names(household), ]
-
-codebookHousehold$question[codebookHousehold$variable == "start"] <- "Start of data collection"
-codebookHousehold$question[codebookHousehold$variable == "end"] <- "End of data collection"
-codebookHousehold$question[codebookHousehold$variable == "today"] <- "Date data collection was initiated"
-
-usethis::use_data(codebookHousehold, overwrite = TRUE)
-
-
-
 ## Merge household identifiers with
-roster <- merge(temp[ , coreColumns], roster, by.x = "KEY", by.y = "PARENT_KEY", all = TRUE)
+roster <- merge(temp[ , coreColumns], temp2, by.x = "KEY", by.y = "PARENT_KEY", all.y = TRUE)
 
+## Remove unnecessary columns with no data
+roster <- subset(roster, select = c(-identify_head, -consent1, -name, -hh_next_member))
+usethis::use_data(roster, overwrite = TRUE)
 
+## Household head
+hhHead <- data.frame(temp[ , coreColumns], temp[ , c(56:69, 79:174, 806:819)])
 
-hhHead        <- data.frame(temp[ , coreColumns], temp[ , c(56:69, 79:174, 806:819)])
-hhOther       <- data.frame(temp[ , coreColumns], temp[ , c(820:943)])
-hhMaleYouth   <- data.frame(temp[ , coreColumns], temp[ , c(944:1060)])
+## Remove unnecessary columns and columns with no data
+hhHead <- subset(hhHead, select = c(-identify_head, -consent1, -sers_instruction,
+                                    -weai_dimension1_instruction,
+                                    -weai_dimension2_instruction,
+                                    -weai_dimension3_instruction,
+                                    -weai_dimension5_instruction,
+                                    -mddw_instruction,
+                                    -end_head_interview))
+
+hhHead$respondent <- 1
+
+hhHead$id <- paste(temp$SET.OF.survey.hh_repeat, "[", temp$hh_repeat_count, "]", sep = "")
+
+names(hhHead) <- c(coreColumns[coreColumns != c("identify_head", "consent1")],
+                   "presence", "missing", "missing_other",
+                   paste("sers", 1:10, sep = ""),
+                   stringr::str_remove(string = names(hhHead)[36:127], pattern = "_head"),
+                   paste("mddw", 1:6, sep = ""), paste("wg", 1:6, sep = ""), "respondent", "id")
+
+## Spouse
+hhOther <- data.frame(temp[ , coreColumns], temp[ , c(820:943)])
+
+## Remove unnecessary columns and columns with no data
+hhOther <- subset(hhOther, select = c(-identify_head, -consent1, -sers_instruction.1,
+                                      -weai_dimension1_instruction.1,
+                                      -weai_dimension2_instruction.1,
+                                      -weai_dimension3_instruction.1,
+                                      -weai_dimension5_instruction.1,
+                                      -mddw_instruction.1,
+                                      -end_other_interview))
+
+hhOther$respondent <- 2
+
+hhOther$id <- paste(temp$SET.OF.survey.hh_repeat, "[", temp$hh_repeat_count, "]", sep = "")
+
+names(hhOther) <- names(hhHead)
+
+## Male youth
+hhMaleYouth <- data.frame(temp[ , coreColumns], temp[ , c(944:1060)])
+
+## Remove unnecessary columns and columns with no data
+hhMaleYouth <- subset(hhMaleYouth, select = c(-identify_head, -consent1, -sers_instruction.2,
+                                              -weai_dimension1_instruction.2,
+                                              -weai_dimension2_instruction.2,
+                                              -weai_dimension3_instruction.2,
+                                              -weai_dimension5_instruction.2,
+                                              -end_myouth_interview))
+
+hhMaleMDDW <- data.frame(matrix(data = NA, nrow = nrow(hhMaleYouth), ncol = 6))
+names(hhMaleMDDW) <- c("mddw1", "mddw2", "mddw3", "mddw4", "mddw5", "mddw6")
+
+hhMaleYouth <- data.frame(hhMaleYouth[ , 1:127], hhMaleMDDW, hhMaleYouth[ , 128:133])
+
+hhMaleYouth$respondent <- 3
+
+hhMaleYouth$id <- paste(temp$SET.OF.survey.hh_repeat, "[", temp$hh_repeat_count, "]", sep = "")
+
+names(hhMaleYouth) <- names(hhHead)
+
+## Female youth
 hhFemaleYouth <- data.frame(temp[ , coreColumns], temp[ , c(1061:1184)])
 
+## Remove unnecessary columns and columns with no data
+hhFemaleYouth <- subset(hhFemaleYouth, select = c(-identify_head, -consent1, -sers_instruction.3,
+                                                  -weai_dimension1_instruction.3,
+                                                  -weai_dimension2_instruction.3,
+                                                  -weai_dimension3_instruction.3,
+                                                  -weai_dimension5_instruction.3,
+                                                  -mddw_instruction.2,
+                                                  -end_fyouth_interview))
 
+hhFemaleYouth$respondent <- 4
+
+hhFemaleYouth$id <- paste(temp$SET.OF.survey.hh_repeat, "[", temp$hh_repeat_count, "]", sep = "")
+
+names(hhFemaleYouth) <- names(hhHead)
+
+individual <- data.frame(rbind(hhHead, hhOther, hhMaleYouth, hhFemaleYouth))
+
+## Add roster information
+individual <- merge(individual, temp2, by.x = "id", by.y = "KEY", all.x = TRUE)
+usethis::use_data(individual, overwrite = TRUE)
 
 
 ################################################################################
@@ -131,8 +201,22 @@ hhFemaleYouth <- data.frame(temp[ , coreColumns], temp[ , c(1061:1184)])
 #
 ################################################################################
 
+## Household
+codebookHousehold <- codebook[codebook$variable %in% names(household), ]
 
+codebookHousehold$question[codebookHousehold$variable == "start"] <- "Start of data collection"
+codebookHousehold$question[codebookHousehold$variable == "end"] <- "End of data collection"
+codebookHousehold$question[codebookHousehold$variable == "today"] <- "Date data collection was initiated"
 
+usethis::use_data(codebookHousehold, overwrite = TRUE)
+
+## Roster
+codebookRoster <- codebook[codebook$variable %in% names(roster), ]
+usethis::use_data(codebookRoster, overwrite = TRUE)
+
+## Individual
+codebookIndividual <- codebook[codebook$variable %in% names(individual), ]
+usethis::use_data(codebookIndividual, overwrite = TRUE)
 
 
 codeHead <- survey[survey$name %in% names(hhHead), c("name", "label")]
