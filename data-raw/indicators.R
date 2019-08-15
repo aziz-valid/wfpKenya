0beb0973ffc3855ad923e3040cebe12b623ed9b7
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-08-15 23:38:36 +0100

    update indicators.R

--- data-raw/indicators.R
+++ data-raw/indicators.R
@@ -1,5 +1,14 @@
 ################################################################################
 #
-#
+# Create SERS indicator dataset
 #
 ################################################################################
+
+coreVars <- c("vid", "KEY", "sex", "hh_member_age", "respondent", "studyGroup", "lhz")
+
+sers <- data.frame(individual[ , coreVars], calculate_sers(df = individual))
+names(sers) <- c("psu", "hhid", "sex", "age", "respondent", "studyGroup", "lhz",
+                 paste("sers", 1:10, sep = ""), "score", "resilience")
+
+usethis::use_data(sers, overwrite = TRUE)
+
