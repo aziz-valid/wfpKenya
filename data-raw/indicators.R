1213eaf0474a0fcca423973abe219ddfc3129c86
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-08-16 00:06:05 +0100

    update indicators.R

--- data-raw/indicators.R
+++ data-raw/indicators.R
@@ -1,14 +1,28 @@
+## Core variables
+coreVars <- c("vid", "KEY", "sex", "hh_member_age", "respondent", "studyGroup", "lhz")
+
 ################################################################################
 #
 # Create SERS indicator dataset
 #
 ################################################################################
 
-coreVars <- c("vid", "KEY", "sex", "hh_member_age", "respondent", "studyGroup", "lhz")
-
 sers <- data.frame(individual[ , coreVars], calculate_sers(df = individual))
 names(sers) <- c("psu", "hhid", "sex", "age", "respondent", "studyGroup", "lhz",
                  paste("sers", 1:10, sep = ""), "score", "resilience")
 
 usethis::use_data(sers, overwrite = TRUE)
+
+
+################################################################################
+#
+# Calculate disability indicators dataset
+#
+################################################################################
+
+wg <- data.frame(individual[ , coreVars], calculate_wg(df = individual))
+names(wg) <- c("psu", "hhid", "sex", "age", "respondent", "studyGroup", "lhz",
+               names(wg)[8:ncol(wg)])
+
+usethis::use_data(wg, overwrite = TRUE)
 
